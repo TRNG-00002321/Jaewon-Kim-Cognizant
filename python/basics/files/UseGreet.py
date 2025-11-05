9e082a8d3a2d3b3cba7376400962cbd6c1da9d48
Jaewon <jaewon.kim1326@gmail.com>
2025-11-05 14:04:30 -0600

    finished first assignment

---
+++ python/basics/files/UseGreet.py
@@ -0,0 +1,2 @@
+from Greet import greet
+print(greet("jeb"))
--- python/inheritancedemo.py
+++
@@ -1,7 +0,0 @@
-class Animal:
-    def speak(self):
-        print("Animal Speaks")
-
-class Dog(Animal):
-    def speak(self):
-        print("Dog Speaks")
